9610ac8be72ba6a7b32ab9cec8287350ad2e002b
CesarCota <cesarmiguelcota@gmail.com>
2018-01-26 03:27:11 -0100

    working on controllers

--- src/main/java/org/academiadecodigo/bootcamp/Controllers/DashboardController.java
+++ src/main/java/org/academiadecodigo/bootcamp/Controllers/DashboardController.java
@@ -1,5 +1,12 @@
 package org.academiadecodigo.bootcamp.Controllers;
 
+import org.academiadecodigo.bootcamp.Models.Group;
+import org.academiadecodigo.bootcamp.Services.GroupService;
+import org.academiadecodigo.bootcamp.Services.JPAGroupService;
+import org.academiadecodigo.bootcamp.Services.JPAUserService;
+import org.academiadecodigo.bootcamp.Services.UserService;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,18 +15,35 @@ import java.io.IOException;
 
 public class DashboardController extends HttpServlet {
 
+    private GroupService userService;
+    private RequestDispatcher pageDispatcher;
+
+    @Override
+    public void init() throws ServletException {
+
+        pageDispatcher = getServletContext().getRequestDispatcher("/WEB-INF/dashboard.jsp");
+        this.userService = (JPAGroupService) getServletContext().getAttribute("GroupService");
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Object user = req.getSession().getAttribute("user");
+
+        if (user == null){
+            pageDispatcher = getServletContext().getRequestDispatcher("/WEB-INF/login.jsp");
+            pageDispatcher.forward(req,resp);
+            return;
+        }
+        pageDispatcher.forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+
+
+
+
     }
 
-    @Override
-    public void init() throws ServletException {
-
-    }
 
 }
